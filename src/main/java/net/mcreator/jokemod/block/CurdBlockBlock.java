f7808611edb50731fde444c18cbcc1379e4328b7
ClassicChair <DJ_Czychacz@192.168.1.6>
2026-06-14 17:03:07 +0200

    huh

--- src/main/java/net/mcreator/jokemod/block/CurdBlockBlock.java
+++ src/main/java/net/mcreator/jokemod/block/CurdBlockBlock.java
@@ -1,6 +1,9 @@
 package net.mcreator.jokemod.block;
 
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
 
 public class CurdBlockBlock extends Block {
 	public CurdBlockBlock(BlockBehaviour.Properties properties) {
