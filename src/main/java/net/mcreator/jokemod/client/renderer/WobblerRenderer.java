c716f6adccc6dd9ca3fa09f15a066ea8d7bf50f3
ClassicChair <DJ_Czychacz@192.168.1.6>
2026-06-15 21:44:45 +0200

    changed bleeding a little

--- src/main/java/net/mcreator/jokemod/client/renderer/WobblerRenderer.java
+++ src/main/java/net/mcreator/jokemod/client/renderer/WobblerRenderer.java
@@ -1,5 +1,13 @@
 package net.mcreator.jokemod.client.renderer;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+
+import net.mcreator.jokemod.entity.WobblerEntity;
+import net.mcreator.jokemod.client.model.Modelwobbler;
+
 public class WobblerRenderer extends MobRenderer<WobblerEntity, LivingEntityRenderState, Modelwobbler> {
 	private WobblerEntity entity = null;
 
